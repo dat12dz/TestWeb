87cfc6b3904e49bf6b9a0c1a12ff788618e531b7
dat12dz <datsieusaiya12@gmail.com>
2022-08-04 15:39:51 +0700

    FirstCommit

---
+++ Villa Web/Villa Web/Controllers/example.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Villa_Web.Controllers
+{
+    public class example : Controller
+    {
+        public IActionResult Index(int l)
+        {
+            ViewResult a = new ViewResult();
+            
+               return View();
+            return Content(l.ToString());
+        }
+        public IActionResult
+
+    }
+}
